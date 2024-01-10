cb396a5eb9bb82850369a911b2969643b0239cfe
Dmytro Momot <momotdev@gmail.com>
2024-01-10 07:02:52 +0200

    feat: implemented navigation and button components

---
+++ src/components/BasicButton/BasicButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BasicButton = ({children}) => {
+    return (
+        <button className="button">
+            {children}
+        </button>
+    );
+};
+
+export default BasicButton;
