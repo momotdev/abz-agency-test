c4ac7ed40e770f9d49d12a7bdda6bc2b314d5d16
Dmytro Momot <momotdev@gmail.com>
2024-01-11 16:30:44 +0200

    fix: type error

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -3,7 +3,7 @@ import './styles/user-card.scss';
 import NoImage from './assets/photo-cover.svg';
 
 const UserCard = ({photo, name, position, email, phone}) => {
-  const [imgSrc, setImgSrc] = useState(photo);
+  const [imgSrc, setImgSrc] = useState(photo ?? '');
   return (
       <div className="user-card">
         <div className="user-card__image-wrapper">
