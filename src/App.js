38df9dacb414de18a05cbf4386e6cc54487bee86
Dmytro Momot <momotdev@gmail.com>
2024-01-11 06:49:03 +0200

    feat: add react-query lib

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './styles/index.scss';
 import Layout from "./components/Layout/Layout";
 import MainBanner from "./components/MainBanner/MainBanner";
 import GetRequestSection from "./components/GetRequestSection/GetRequestSection";
+import {QueryClient, QueryClientProvider} from "react-query";
+
+const queryClient = new QueryClient()
 
 // TODO: FIX INDENTS
 function App() {
   return (
-      <Layout>
-        <MainBanner/>
-        <GetRequestSection/>
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <MainBanner/>
+          <GetRequestSection/>
+        </Layout>
+      </QueryClientProvider>
   );
 }
 
